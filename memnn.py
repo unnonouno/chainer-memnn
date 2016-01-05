88bbaa8a59935dd7603b151849dc8d21ec946055
Yuya Unno <unnonouno@gmail.com>
2016-01-05 20:43:29 +0900

    Use batch learning

--- memnn.py
+++ memnn.py
@@ -1,3 +1,4 @@
+import copy
 import glob
 import random
 
@@ -10,21 +11,22 @@ import cupy
 
 import nlputil
 
-import dot
 
-
-def _encode(embed, sentences, length, position_encoding=True):
+def _encode(embed, sentences, length, position_encoding=False):
     e = embed(chainer.Variable(sentences))
     if position_encoding:
+        ndim = e.data.ndim
         n_batch = e.data.shape[0]
         n_words = e.data.shape[1]
         n_units = e.data.shape[2]
-        length = length.reshape(n_batch, 1, 1).astype(numpy.float32)
-        k = (xp.arange(1, n_units + 1, dtype=numpy.float32) / n_units).reshape(1, 1, n_units)
-        i = (xp.arange(1, n_words + 1, dtype=numpy.float32)).reshape(1, n_words, 1)
+        length = length.reshape((n_batch,) + (1,) * (ndim - 1)).astype(numpy.float32)
+        k = xp.arange(1, n_units + 1, dtype=numpy.float32) / n_units
+        k = k.reshape((1,)*(ndim-2) +  (1, n_units))
+        i = xp.arange(1, n_words + 1, dtype=numpy.float32)
+        i = i.reshape((1,)*(ndim-2) +  (n_words, 1))
         coeff = (1 - i / length) - k * (1 - 2.0 * i / length)
         e = chainer.Variable(coeff) * e
-    s = F.sum(e, axis=1)
+    s = F.sum(e, axis=-2)
     return s
 
 
@@ -35,28 +37,18 @@ class Memory(object):
         self.C = C
         self.TA = TA
         self.TC = TC
-        self.ms = []
-        self.cs = []
-
-    def reset_state(self):
-        self.ms = []
-        self.cs = []
 
     def encode(self, sentence, lengths):
         mi = _encode(self.A, sentence, lengths)
         ci = _encode(self.C, sentence, lengths)
         return mi, ci
 
-    def register(self, sentence, lengths):
-        mi, ci = self.encode(sentence, lengths)
-        mi = F.reshape(mi, (mi.data.shape[0], 1, mi.data.shape[1]))
-        ci = F.reshape(ci, (ci.data.shape[0], 1, ci.data.shape[1]))
-        self.ms.append(mi)
-        self.cs.append(ci)
+    def register_all(self, sentences, lengths=None):
+        self.m, self.c = self.encode(sentences, lengths)
 
     def query(self, u):
-        m = F.concat(self.ms)
-        c = F.concat(self.cs)
+        m = self.m
+        c = self.c
         batch = m.data.shape[0]
         size = m.data.shape[1]
         inds, _ = xp.broadcast_arrays(
@@ -65,8 +57,9 @@ class Memory(object):
         inds = chainer.Variable(inds)
         tm = self.TA(inds)
         tc = self.TC(inds)
-        p = F.softmax(dot.dot(u, m + tm))
-        o = dot.dot(p, F.swapaxes(c + tc, 2, 1))
+        p = F.softmax(F.batch_matmul(m + tm, u))
+        o = F.batch_matmul(F.swapaxes(c + tc, 2, 1), p)
+        o = F.reshape(o, (batch, m.data.shape[2]))
         u = o + u
         return u
 
@@ -104,15 +97,19 @@ class MemNN(chainer.Chain):
         init_params(self.E1, self.E2, self.E3, self.E4,
                     self.T1, self.T2, self.T3, self.T4)
 
-    def reset_state(self):
-        self.M1.reset_state()
-        self.M2.reset_state()
-        self.M3.reset_state()
+    def fix_ignore_label(self):
+        for embed in [self.E1, self.E2, self.E3, self.E4]:
+            embed.W.data[0, :] = 0
 
     def register(self, sentence, lengths):
         self.M1.register(sentence, lengths)
         self.M2.register(sentence, lengths)
         self.M3.register(sentence, lengths)
+
+    def register_all(self, sentences, lengths):
+        self.M1.register_all(sentences, lengths)
+        self.M2.register_all(sentences, lengths)
+        self.M3.register_all(sentences, lengths)
 
     def query(self, question, lengths, y):
         u = _encode(self.B, question, lengths)
@@ -149,41 +146,69 @@ def proc(proc_data, batch_size, train=True):
     for begin in range(0, len(proc_data), batch_size):
         end = min(len(proc_data), begin + batch_size)
         batch_data = proc_data[begin:end]
-        max_len = max(len(d) for d in batch_data)
-        model.reset_state()
+
         accum_loss = None
-        for i in range(max_len):
-            lines = [d[i] for d in batch_data]
-            sentences, lengths = make_batch_sentence(lines)
 
-            if all(isinstance(line, data.Sentence) for line in lines):
-                model.register(sentences, lengths)
-            elif all(isinstance(line, data.Query) for line in lines):
-                y_data = xp.array([line.answer for line in lines], dtype=numpy.int32)
-                y = chainer.Variable(y_data)
-                loss, acc = model.query(sentences, lengths, y)
+        mem = xp.concatenate([mem.reshape((1, mem.shape[0],  mem.shape[1]))
+                              for mem, _, _ in batch_data])
+        query = xp.concatenate([query.reshape((1, query.shape[0]))
+                                for _, query, _ in batch_data])
+        answer = xp.array([answer for _, _, answer in batch_data], dtype=numpy.int32)
 
-                if train:
-                    if accum_loss is None:
-                        accum_loss = loss
-                    else:
-                        accum_loss += loss
+        model.register_all(mem, None)
 
-                total_loss += loss.data
-                total_acc += acc.data
-                count += 1
+        y = chainer.Variable(answer)
+        loss, acc = model.query(query, None, y)
+
+        if train:
+            if accum_loss is None:
+                accum_loss = loss
             else:
-                raise
+                accum_loss += loss
+
+        total_loss += loss.data
+        total_acc += acc.data
+        count += 1
 
         if accum_loss is not None:
             model.zerograds()
             accum_loss.backward()
             opt.update()
-            for link in model.links():
-                if isinstance(link, L.EmbedID):
-                    link.W.data[0, :] = 0
+            model.fix_ignore_label()
 
-    print('loss: %.4f\tacc: %.2f' % (float(total_loss), float(total_acc) / count * 100))
+    #print('loss: %.4f\tacc: %.2f' % (float(total_loss), float(total_acc) / count * 100))
+    return float(total_acc) / count
+
+
+def convert_data(train_data, gpu):
+    d = []
+    sentence_len = max(max(len(s.sentence) for s in story) for story in train_data)
+    for story in train_data:
+        mem = numpy.zeros((50, sentence_len), dtype=numpy.int32)
+        mem_length = numpy.zeros((50,), dtype=numpy.int32)
+        i = 0
+        for sent in story:
+            if isinstance(sent, data.Sentence):
+                if i == 50:
+                    mem[0:i-1, :] = mem[1:i, :]
+                    mem_length[0:i-1] = mem_length[1:i]
+                    i -= 1
+                mem[i, 0:len(sent.sentence)] = sent.sentence
+                mem_length[i] = len(sent.sentence)
+                i += 1
+            elif isinstance(sent, data.Query):
+                query = numpy.zeros(sentence_len, dtype=numpy.int32)
+                query[0:len(sent.sentence)] = sent.sentence
+                if gpu >= 0:
+                    d.append((chainer.cuda.to_gpu(mem),
+                              chainer.cuda.to_gpu(query),
+                              sent.answer))
+                else:
+                    d.append((copy.deepcopy(mem),
+                              (query),
+                              sent.answer))
+
+    return d
 
 
 if __name__ == '__main__':
@@ -192,34 +217,42 @@ if __name__ == '__main__':
     vocab = nlputil.Vocabulary()
     data_dir = '/home/unno/qa/tasks_1-20_v1-2'
     data_type = 'en'
-    data_id = 1
+    for data_id in range(1, 21):
 
-    train_data = data.read_data(
-        vocab,
-        glob.glob('%s/%s/qa%d_*train.txt' % (data_dir, data_type, data_id))[0])
-    test_data = data.read_data(
-        vocab,
-        glob.glob('%s/%s/qa%d_*test.txt' % (data_dir, data_type, data_id))[0])
-    print('Training data: %d' % len(train_data))
+        train_data = data.read_data(
+            vocab,
+            glob.glob('%s/%s/qa%d_*train.txt' % (data_dir, data_type, data_id))[0])
+        test_data = data.read_data(
+            vocab,
+            glob.glob('%s/%s/qa%d_*test.txt' % (data_dir, data_type, data_id))[0])
+        print('Training data: %d' % len(train_data))
 
-    model = MemNN(20, vocab.size)
-    opt = optimizers.Adam()
-    #opt = optimizers.SGD(lr=0.01)
-    #opt.add_hook(chainer.optimizer.GradientClipping(40))
-    batch_size = 500
-    gpu = 0
-    if gpu >= 0:
-        model.to_gpu()
-        xp = cupy
-    else:
-        xp = numpy
-    opt.setup(model)
+        gpu = 0
+        train_data = convert_data(train_data, gpu)
+        test_data = convert_data(test_data, gpu)
 
-    for epoch in range(1000):
-        if isinstance(opt, optimizers.SGD) and epoch % 25 == 24:
-            opt.lr *= 0.5
-        print(epoch)
+        model = MemNN(20, vocab.size, 50)
+        opt = optimizers.Adam()
+        #opt = optimizers.SGD(lr=0.01)
+        #opt.add_hook(chainer.optimizer.GradientClipping(40))
+        batch_size = 100
+        numpy.seterr('raise')
+        if gpu >= 0:
+            model.to_gpu()
+            xp = cupy
+        else:
+            xp = numpy
+        opt.setup(model)
 
-        random.shuffle(train_data)
-        proc(train_data, batch_size, train=True)
-        proc(test_data, batch_size, train=False)
+        for epoch in range(1000):
+            if isinstance(opt, optimizers.SGD) and epoch % 25 == 24:
+                opt.lr *= 0.5
+            #print(epoch)
+
+            random.shuffle(train_data)
+            proc(train_data, batch_size, train=True)
+            acc = proc(test_data, batch_size, train=False)
+
+        acc = acc * 100
+        err = 100 - acc
+        print('%d: acc: %.2f\terr: %.2f' % (data_id, acc, err))
